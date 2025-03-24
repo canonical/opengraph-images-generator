7f51502ee2439f3ee94820fc7ad1c459dd52c49f
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-03-24 01:30:47 +0100

    feat: update Card component to support images

--- src/lib/components/card/Card.svelte
+++ src/lib/components/card/Card.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
 	import clsx from 'clsx';
+	import type { Snippet } from 'svelte';
 	import type { HTMLAttributes } from 'svelte/elements';
 
 	type CardProps = {
 		highlighted?: boolean;
 		overlay?: boolean;
 		title?: string;
+		image?: Snippet;
 	} & HTMLAttributes<HTMLElement>;
 
 	let {
 		title,
+		image,
 		children,
 		highlighted = false,
 		overlay = false,
 		class: className,
 		...restProps
 	}: CardProps = $props();
+
+	const contentClass = image ? 'p-card__inner' : 'p-card__content';
 </script>
 
 <div
@@ -30,13 +35,24 @@
 	role="region"
 	{...restProps}
 >
-	{#if title}
-		<h3 class="p-card__title">
-			{title}
-		</h3>
+	{#if image}
+		<div class="p-card__thumbnail">
+			{@render image?.()}
+		</div>
+		<hr />
 	{/if}
-
-	<div class="p-card__content">
+	<div
+		class={clsx({
+			[contentClass]: image
+		})}
+	>
+		{#if title}
+			<h3 class="p-card__title">
+				{title}
+			</h3>
+		{/if}
+	</div>
+	<div class={contentClass}>
 		{@render children?.()}
 	</div>
 </div>
